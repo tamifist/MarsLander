dac9a566c0d14afd7b33ed231abb4aa9c7de9230
Vlad Omelyanchuk <v.omelyanchuk@outlook.com>
2018-12-02 20:55:48 +0300

    Add project files.

---
+++ MarsLanderE3GA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarsLanderE3GA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
